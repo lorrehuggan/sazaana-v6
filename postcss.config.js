2a1ba43ec0145b2f7aece13603716afece6f6bde
Lorre Huggan <lorre.huggan@protonmail.com>
2023-12-04 20:05:50 +0000

    edit placeholder

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 const config = {
   plugins: {
+    autoprefixer: {},
     'postcss-nested': {},
     'postcss-preset-env': {
       stage: 3,
     },
     'postcss-easing-gradients': {},
-    autoprefixer: {},
     cssnano: {},
   },
 };
